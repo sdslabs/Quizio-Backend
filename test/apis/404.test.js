5abf5cc5908028b423cd74479c3259579781a749
CapriciousRebel <code.capriciousrebel@gmail.com>
2021-12-26 22:46:41 +0530

    [Feature]: Add httpstatuscodes library

--- test/apis/404.test.js
+++ test/apis/404.test.js
@@ -1,11 +1,12 @@
 import request from 'supertest';
+import { StatusCodes } from 'http-status-codes';
 import app from '../../src/app';
 
 describe('GET /404', () => {
 	describe('should return a 404 response', () => {
 		test('should respond with a 404 status code', async () => {
 			const response = await request(app).get('/404');
-			expect(response.statusCode).toBe(404);
+			expect(response.statusCode).toBe(StatusCodes.NOT_FOUND);
 		});
 		test('response type should be json, utf-8 encoded', async () => {
 			const response = await request(app).get('/404');
@@ -18,7 +19,7 @@ describe('POST /404', () => {
 	describe('should return a 404 response', () => {
 		test('should respond with a 404 status code', async () => {
 			const response = await request(app).post('/404');
-			expect(response.statusCode).toBe(404);
+			expect(response.statusCode).toBe(StatusCodes.NOT_FOUND);
 		});
 		test('response type should be json, utf-8 encoded', async () => {
 			const response = await request(app).post('/404');
@@ -31,7 +32,7 @@ describe('DELETE /404', () => {
 	describe('should return a 404 response', () => {
 		test('should respond with a 404 status code', async () => {
 			const response = await request(app).delete('/404');
-			expect(response.statusCode).toBe(404);
+			expect(response.statusCode).toBe(StatusCodes.NOT_FOUND);
 		});
 		test('response type should be json, utf-8 encoded', async () => {
 			const response = await request(app).delete('/404');
@@ -44,7 +45,7 @@ describe('PUT /404', () => {
 	describe('should return a 404 response', () => {
 		test('should respond with a 404 status code', async () => {
 			const response = await request(app).put('/404');
-			expect(response.statusCode).toBe(404);
+			expect(response.statusCode).toBe(StatusCodes.NOT_FOUND);
 		});
 		test('response type should be json, utf-8 encoded', async () => {
 			const response = await request(app).put('/404');
@@ -57,7 +58,7 @@ describe('PATCH /404', () => {
 	describe('should return a 404 response', () => {
 		test('should respond with a 404 status code', async () => {
 			const response = await request(app).patch('/404');
-			expect(response.statusCode).toBe(404);
+			expect(response.statusCode).toBe(StatusCodes.NOT_FOUND);
 		});
 		test('response type should be json, utf-8 encoded', async () => {
 			const response = await request(app).patch('/404');
--- test/apis/static.test.js
+++ test/apis/static.test.js
@@ -1,11 +1,12 @@
 import request from 'supertest';
+import { StatusCodes } from 'http-status-codes';
 import app from '../../src/app';
 
 describe('GET /', () => {
 	describe('should return a html file with info about quizio', () => {
 		test('should respond with a 200 status code', async () => {
 			const response = await request(app).get('/');
-			expect(response.statusCode).toBe(200);
+			expect(response.statusCode).toBe(StatusCodes.OK);
 		});
 		test('response type should be html', async () => {
 			const response = await request(app).get('/');
