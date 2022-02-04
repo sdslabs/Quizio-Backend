2d75997e17da4cf667957bcd22cadc9356d50b39
pathange-s <sainatharao@gmail.com>
2022-02-04 15:41:09 +0530

    [Feature]: Add is submit check when user saves response

--- src/controllers/response.js
+++ src/controllers/response.js
@@ -4,15 +4,16 @@ import {
 	successResponseWithData,
 } from '../helpers/responses';
 import { getQuestionByID } from '../models/question';
-
 import { saveResponse } from '../models/response';
+import { isUserSubmitted } from '../models/submit';
 
 const controller = {
 	saveResponse: async (req, res) => {
 		const { username } = req.user;
 		const responseData = { ...req.body, username };
 		const questionExists = await getQuestionByID(responseData.questionID);
-		if (questionExists) {
+		const isSubmitted = await isUserSubmitted(username);
+		if (questionExists && !isSubmitted) {
 			const response = await saveResponse(responseData);
 			return response ? successResponseWithData(res, {
 				msg: 'Response saved',
