a275ed119c961c94175cc0a5240a11c6e9c4dfc9
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-02-13 17:37:43 +0530

    Completed submission check for response route

--- src/controllers/response.js
+++ src/controllers/response.js
@@ -13,16 +13,14 @@ const controller = {
 	saveResponse: async (req, res) => {
 		const { username } = req.user;
 		const responseData = { ...req.body, username };
-		console.log(responseData);
 		const questionExists = await getQuestionByID(responseData.questionID);
 		if (questionExists) {
 			const sectionData = await getSectionByID(questionExists.sectionID);
 			const submitExits = await checkSubmit(sectionData.quizID, username);
 			if (submitExits) {
-				return "You have already submitted this quiz";
+				return errorResponse(res, 'Quiz already submitted!');
 			}
 			const response = await saveResponse(responseData);
-			console.log(response);
 			return response ? successResponseWithData(res, {
 				msg: 'Response saved',
 				response,
