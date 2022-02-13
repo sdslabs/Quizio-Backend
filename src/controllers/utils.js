d226537006076ca80caabcc397b7579bca46a3ad
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-02-13 22:52:33 +0530

    create upload image route

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -1,5 +1,9 @@
 import { failureResponseWithMessage, successResponseWithMessage } from '../helpers/responses';
-import { verifyQuizioID } from '../helpers/utils';
+import { verifyQuizioID, generateQuizioID } from '../helpers/utils';
+import axios from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
+
 
 const controller = {
 
@@ -10,6 +14,28 @@ const controller = {
 			? successResponseWithMessage(res, 'ID is a valid quizio document ID, but is not guaranteed to be an actual ID in db')
 			: failureResponseWithMessage(res, 'ID is NOT a valid quizio document ID, the format is `quizioID.{nanoid())}`');
 	},
+	
+	uploadImage: async (req, res) => {
+		const data = req;
+		console.log(req.file);
+		console.log(req.body);
+
+		const form = new FormData();
+		form.append('image', fs.createReadStream(req.file.path));
+		form.append('name', generateQuizioID());
+		form.append('key', '5f8bf99f5a3c8ddac572f7fb2f3835c4');
+		
+		console.log(form);
+		const apiResponse = await axios.post('https://api.imgbb.com/1/upload',
+			form,
+			{
+				headers: {
+					'Content-Type': `multipart/form-data; boundary=${form._boundary}`,
+				}
+			});
+		console.log(apiResponse.data);
+		return failureResponseWithMessage(res, 'ID is NOT a valid quizio document ID, the format is `quizioID.{nanoid())}`');
+	}
 
 };
 
--- src/routers/utils.js
+++ src/routers/utils.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 import { isAuth } from '../helpers/authorizer';
 import controller from '../controllers/utils';
+import multer from 'multer';
+
+// const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
 router.get('/verifyQuizioID/:id', isAuth, controller.verifyQuizioID); // DONE
+
+router.post('/uploadImage', [isAuth, upload.single('image')], controller.uploadImage);
 export default router;
