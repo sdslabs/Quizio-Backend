4ff8c080260dbaf0641a79916ef20148d6f13029
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-03-16 13:56:11 +0530

    [Refactor, Feature]: timer service code and add table display for testing timer

--- src/services/timerService.js
+++ src/services/timerService.js
@@ -7,61 +7,53 @@ let removeFromOngoingQuizzes = [];
 
 export const removeOngoingQuizFromTimer = (quizioID) => {
 	const index = ongoingQuizzes.findIndex((quiz) => quiz.quizioID === quizioID);
-	if (index !== -1) {
-		removeFromOngoingQuizzes.push(quizioID);
-	}
+	if (index !== -1) removeFromOngoingQuizzes.push(quizioID);
 };
 
+/**
+ * @returns All upcoming quizzes
+ */
 const getUpcomingQuizzes = async () => {
-	// console.log('dayjs()', dayjs().format().toLocaleString());
 	const quizzes = await getAllQuizzes();
 
 	return quizzes.filter((quiz) => {
-		// const startTime = dayjs(quiz.startTime);
 		const endTime = dayjs(quiz.endTime);
 		const now = dayjs();
-
-		// const startTimeString = startTime.toString();
-		// const endTimeString = endTime.toString();
-		// const nowString = now.toString();
-
-		// if (endTime.isAfter(now)) {
-		// 	console.log({
-		// 		quizID: quiz.quizioID,
-		// 		startTimeString,
-		// 		endTimeString,
-		// 		nowString,
-		// 		ongoing: (startTime <= now) && (now <= endTime),
-		// 		timeLeft: dayjs(quiz.endTime).diff(now, 'seconds'),
-		// 		locale: dayjs.locale(),
-		// 	});
-		// }
 		return endTime.isAfter(now);
 	}).map((quiz) => ({
 		quizioID: quiz.quizioID,
 		time: dayjs(quiz.endTime).diff(dayjs(), 'seconds'),
 		startTime: quiz.startTime,
+		name: quiz.name,
 	}));
 };
 
+/**
+ * Generates a timer service
+ * @param {*} io The socket object
+ */
 const timerService = async (io) => {
 	let clients = 0;
 	logger.info('TIMER SERVICE: INITIATE');
 	let upcomingQuizzes = [];
 
+	// Update upcoming quizzes every second
 	setInterval(async () => {
 		upcomingQuizzes = await getUpcomingQuizzes();
 	}, 1000);
 
+	// Emit timer for ongoing quizzes every second
 	setInterval(() => {
 		io.sockets.emit('quizTimer', ongoingQuizzes);
 
+		// Remove all quizzes marked for removal from ongoing quizzes
 		if (removeFromOngoingQuizzes.length > 0) {
 			ongoingQuizzes = ongoingQuizzes
 				.filter((quiz) => !removeFromOngoingQuizzes.includes(quiz.quizioID));
 			removeFromOngoingQuizzes = [];
 		}
 
+		// Add new upcoming quizzes
 		const newAddition = upcomingQuizzes
 			.filter((quiz) => !ongoingQuizzes
 				.some((ongoingQuiz) => ongoingQuiz.quizioID === quiz.quizioID)
@@ -69,20 +61,23 @@ const timerService = async (io) => {
 			.map((quiz) => ({
 				quizioID: quiz.quizioID,
 				time: quiz.time,
+				name: quiz.name,
 			}));
 
 		ongoingQuizzes = ongoingQuizzes.concat(newAddition);
 
+		// Update remaining time on ongoing quizzes
 		for (let i = 0; i < ongoingQuizzes.length; i += 1) {
 			ongoingQuizzes[i].time -= 1;
 			if (ongoingQuizzes[i].time <= 0) {
 				ongoingQuizzes.splice(i, 1);
 			}
 		}
-		// logger.info('TIMER SERVICE: UPDATE');
-		// logger.info(ongoingQuizzes);
+		logger.info('TIMER SERVICE: UPDATE');
+		logger.info(ongoingQuizzes);
 	}, 1000);
 
+	// Manage socket connection and disconnect
 	io.on('connection', (socket) => {
 		clients += 1;
 		socket.on('disconnect', () => {
